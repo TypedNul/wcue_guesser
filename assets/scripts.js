70e125efac710b59b68fb16fd3afeaef96f245ed
Typed Null <ghostdarktype@gmail.com>
2024-04-04 01:32:38 +0200

    commit #4.1, corrections again

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,7 +4,7 @@ fetch('assets/database/database.json')
   .then(response => response.json()) // Converte a resposta para JSON
   .then(data => {
     const infoElement = document.getElementById('info')
-    infoElement.textContent = '${data.imgs}, ${data.nomes}'
+    infoElement.textContent = `${data.imgs}, ${data.nomes}`;
 
   })
   .catch(error => console.error("Erro carregando database JSON: ", error))
