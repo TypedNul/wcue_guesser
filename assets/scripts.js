13a96c95720a5f873a20ddaf16ea02b9bd5dfbdc
Typed Null <ghostdarktype@gmail.com>
2024-04-04 01:39:18 +0200

    commit #4.2, corrections

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,7 +4,12 @@ fetch('assets/database/database.json')
   .then(response => response.json()) // Converte a resposta para JSON
   .then(data => {
     const infoElement = document.getElementById('info')
-    infoElement.textContent = `${data.imgs}, ${data.nomes}`;
+    const data_pars = JSON.parse(data);
+
+    const randomIndex = Math.floor(Math.random() * data_pars.names.length);
+    const randomValue = data_pars[randomIndex].name;
+
+    console.log(randomValue); 
 
   })
   .catch(error => console.error("Erro carregando database JSON: ", error))
