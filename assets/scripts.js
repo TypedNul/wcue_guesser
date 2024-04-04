fc96dcb2f061cdbb3a877720b6c93b0a25fc6e6b
Typed Null <ghostdarktype@gmail.com>
2024-04-04 02:23:54 +0200

    aaaaaa

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,8 @@
 function randomizar(lista, butao) {
   const randIndex = Math.floor(Math.random() * lista.length);
   butao.innerHTML = lista[randIndex];
-}
 
+}
 
 // achar listas do .json
 
@@ -10,10 +10,14 @@ fetch('assets/database/database.json')
   .then(response => response.json()) 
   .then(data => {
     const image = document.getElementById('image')
-   
-    randomizar(data.nomes, document.getElementById('butao1'))
-    randomizar(data.nomes, document.getElementById('butao2'))
-    randomizar(data.nomes, document.getElementById('butao3'))
+    const butao1 = document.getElementById('butao1') 
+    const butao2 = document.getElementById('butao2') 
+    const butao3 = document.getElementById('butao3')
+    
+    while (butao1.textContent !== butao2.textContent || butao2.textContent !== butao3.textContent || butao1.textContent !== butao3.textContent) {
+      randomizar(data.nomes, document.getElementById('butao1'))
+      randomizar(data.nomes, document.getElementById('butao2'))
+      randomizar(data.nomes, document.getElementById('butao3'))
     
 
     const randIndex2 = Math.floor(Math.random() * data.imgs.length);
