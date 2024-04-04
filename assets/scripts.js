011d4a8f7c7f1f8e38a94dea208fad59df0cf861
Typed Null <ghostdarktype@gmail.com>
2024-04-04 02:06:44 +0200

    funciona pfv

--- assets/scripts.js
+++ assets/scripts.js
@@ -15,8 +15,8 @@ fetch('assets/database/database.json')
     const randNome2 = data.imgs[randIndex2];
 
     console.log(randNome2)
-
-    image.textContent = randNome2 
+    
+    image.src = `assets/database/images/${randNome2}` 
     info.textContent = randNome1 
 
 
