814c42ce186e6fe5f73fb6782cc0385aee23ba3b
Typed Null <ghostdarktype@gmail.com>
2024-04-04 17:34:31 +0200

    asdasdsadsa

--- assets/scripts.js
+++ assets/scripts.js
@@ -67,7 +67,7 @@ function fetchar() {
 
 function checarResposta(id) {
   const butaoApertado = document.getElementById(id)
-  if butaoApertado.id = id:
+  if (butaoApertado.id = id):
     respostas_corretas += 1
     fetchar()
   else: 
