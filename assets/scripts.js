f2a1543c9832fc57fc8f64a7f35f3fa9e7c7d0a2
Typed Null <ghostdarktype@gmail.com>
2024-04-04 02:15:44 +0200

    tomara q de

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,20 @@
+function randomizar(lista, butao) {
+  const randIndex = Math.floor(Math.random() * lista.length);
+  butao.textContent = lista[randIndex];
+}
+
+
 // achar listas do .json
 
 fetch('assets/database/database.json')
   .then(response => response.json()) 
   .then(data => {
     const image = document.getElementById('image')
-    const info = document.getElementById('info')
-
-    const randIndex1 = Math.floor(Math.random() * data.nomes.length);
-    const randNome1 = data.nomes[randIndex1];
-
-    console.log(randNome1); 
+   
+    randomizar(data.nomes, document.getElementById('butao1'))
+    randomizar(data.nomes, document.getElementById('butao2'))
+    randomizar(data.nomes, document.getElementById('butao3'))
+    
 
     const randIndex2 = Math.floor(Math.random() * data.imgs.length);
     const randNome2 = data.imgs[randIndex2];
