245f2621b4f8adfca485bf8775e5f514fade15a8
Typed Null <ghostdarktype@gmail.com>
2024-04-04 02:49:15 +0200

    tentei

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,31 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+  fetchar()
+
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => {
+    button.addEventListener('click', fetchar);
+  });
+});
+
 function randomizar(lista, butao) {
   const randIndex = Math.floor(Math.random() * lista.length);
   butao.innerHTML = lista[randIndex];
 
 }
 
-// achar listas do .json
-
-fetch('assets/database/database.json')
-  .then(response => response.json()) 
-  .then(data => {
-    const image = document.getElementById('image')
-    const butao1 = document.getElementById('butao1') 
-    const butao2 = document.getElementById('butao2') 
-    const butao3 = document.getElementById('butao3')
+function fetchar() {
+  fetch('assets/database/database.json')
+    .then(response => response.json()) 
+    .then(data => {
+      const image = document.getElementById('image')
+      const butao1 = document.getElementById('butao1') 
+      const butao2 = document.getElementById('butao2') 
+      const butao3 = document.getElementById('butao3')
     
-    while (butao1.textContent == butao2.textContent || butao2.textContent == butao3.textContent || butao1.textContent == butao3.textContent) {
-      randomizar(data.nomes, document.getElementById('butao1'))
-      randomizar(data.nomes, document.getElementById('butao2'))
-      randomizar(data.nomes, document.getElementById('butao3'))
-    }
+      while (butao1.textContent == butao2.textContent || butao2.textContent == butao3.textContent || butao1.textContent == butao3.textContent) {
+        randomizar(data.nomes, document.getElementById('butao1'))
+        randomizar(data.nomes, document.getElementById('butao2'))
+        randomizar(data.nomes, document.getElementById('butao3'))
+      }
 
-    const randIndex2 = Math.floor(Math.random() * data.imgs.length);
-    const randNome2 = data.imgs[randIndex2];
+      const randIndex2 = Math.floor(Math.random() * data.imgs.length);
+      const randNome2 = data.imgs[randIndex2];
 
-    console.log(randNome2)
+      console.log(randNome2)
 
-    image.src = `assets/database/images/${randNome2}.png`  
+      image.src = `assets/database/images/${randNome2}.png`  
 
-  })
-  .catch(error => console.error("Erro carregando database JSON: ", error))
+    })
+    .catch(error => console.error("Erro carregando database JSON: ", error))  
+}
+
+
+
